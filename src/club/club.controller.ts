bac0a669a9bb4534ff2a24b8e26ff436a0cba0d9
stutuman <la12cabj124@hotmail.com>
2026-03-26 22:37:34 -0300

    add call function approveClub

--- src/club/club.controller.ts
+++ src/club/club.controller.ts
@@ -1,8 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req, ParseIntPipe } from '@nestjs/common';
 import { ClubService } from './club.service';
 import { CreateClubDto } from './dto/create-club.dto';
 import { UpdateClubDto } from './dto/update-club.dto';
-import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiParam } from '@nestjs/swagger';
 import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('club')
@@ -16,6 +16,14 @@ export class ClubController {
    @Req() req:any) {
     const userId=req.user.sub;
     return this.clubService.create(createClubDto,userId);
+  }
+  @ApiBearerAuth('access-token')
+  @UseGuards(AuthGuard) // Más adelante le agregaremos un Guard para que solo vos (SUPER_ADMIN) puedas pegarle a esto
+  @Patch(':id/approve')
+  @ApiOperation({ summary: 'Aprobar un club y ascender al creador a OWNER (Uso interno)' })
+  @ApiParam({ name: 'id', description: 'ID del club a aprobar', example: 1 })
+  approveClub(@Param('id', ParseIntPipe) id: number) {
+    return this.clubService.approveClub(id);
   }
 
   @Get()
